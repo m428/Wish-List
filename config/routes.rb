49f2b58742ee6307d8c7682edf4fb4739ef77f31
Mala <m.nimalasuriya@gmail.com>
2016-06-03 20:47:18 -0700

    add items controller and item routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   resources :users
   resources :sessions
+  resources :items
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160602212808) do
+ActiveRecord::Schema.define(version: 20160604034128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.string   "category"
+    t.integer  "cost"
+    t.string   "note"
+    t.string   "seller"
+    t.string   "link"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -29,4 +43,5 @@ ActiveRecord::Schema.define(version: 20160602212808) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "items", "users"
 end
