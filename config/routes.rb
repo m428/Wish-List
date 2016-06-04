0f6fcc6dc3d4b621e7352d15b13aac41fb25c4f6
Mala <m.nimalasuriya@gmail.com>
2016-06-03 19:18:13 -0700

    add login and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   root 'welcome#index'
   get '/sign_up' => 'users#new'
   resources :users
+  resources :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
