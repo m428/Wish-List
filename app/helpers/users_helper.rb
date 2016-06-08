b2947c259a8c07b402c7ba03153546f1245e2c09
Mala <m.nimalasuriya@gmail.com>
2016-06-08 14:58:43 -0700

    begin work on countdown

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,11 @@ module UsersHelper
   def user_params
      params.require(:user).permit(:first_name, :last_name, :username, :birthday, :city, :state, :email, :password, :password_confirmation)
   end
+
+    def countdown
+      dob = @user.birthday
+      now = Time.now.utc.to_date
+      now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+    end
+
 end
