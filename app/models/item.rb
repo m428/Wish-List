ee87a91c9cb2b13fdb9f68b76076beeef9f4d4d8
Mala <m.nimalasuriya@gmail.com>
2016-06-09 14:21:36 -0700

    clean up item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,12 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-
   before_save :default_values
    def default_values
      self.cost ||= 0
    end
-  # scope :user_id, -> (user_id) { where user_id: user_id}
 
   def self.search(search)
       where('title ILIKE ? OR category ILIKE ? OR seller ILIKE ? OR note ILIKE ? OR cast(cost as text) ILIKE?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
   end
-
 end
