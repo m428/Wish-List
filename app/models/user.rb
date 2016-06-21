90fb75e279ab8687e6bed68bac6122dbb5638bb0
Mala <m.nimalasuriya@gmail.com>
2016-06-21 14:02:47 -0700

    add additional styling to user item views, login, and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :items
-  validates :email, :username, :first_name, :last_name, :birthday, :city, :state, presence: true
+  validates :email, :username, :first_name, :last_name, :birthday, presence: true
   validates_confirmation_of :password, message: "should match password", if: :password
 end
