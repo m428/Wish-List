fe8a10bf2729cecf4b6b6a93409878ac2181d4b0
Mala <m.nimalasuriya@gmail.com>
2016-06-25 14:01:28 -0700

    basic build complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :items
-  validates :email, :username, :first_name, :last_name, :birthday, presence: true
+  validates :email, :username, :first_name, :last_name, presence: true
   validates_confirmation_of :password, message: "should match password", if: :password
+
+  def self.search(search)
+      where('first_name ILIKE ? OR last_name ILIKE ? OR username ILIKE ? OR email ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
 end
